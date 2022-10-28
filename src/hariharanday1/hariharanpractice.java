a5cc4dda6cbe18f247a3e6e592bc0f6143b4b548
hariharan.k <hariharanscorpian@gmail.com>
2022-10-28 09:33:11 +0530

    hari project

---
+++ src/hariharanday1/hariharanpractice.java
@@ -0,0 +1,19 @@
+package hariharanday1;
+
+public class hariharanpractice {
+	public void mechanical() {
+		System.out.println("mechanicalengineering");
+	}
+	public void mba() {
+		System.out.println("hrandoperationmanagement");
+	}
+	public static void main(String[] args) {
+		hariharanpractice h= new hariharanpractice ();
+		h.mechanical();
+		h.mba();
+	}
+	 
+		
+	
+
+}
